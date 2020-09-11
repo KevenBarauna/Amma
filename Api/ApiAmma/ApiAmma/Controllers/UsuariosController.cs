e196052606fa74313750ee8deb0b36ecfe9beb3c
Keven Baraúna <kpbarauna>
2020-09-11 11:09:19 -0300

    Usuario Controller/Model

---
+++ Api/ApiAmma/ApiAmma/Controllers/UsuariosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiAmma.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAmma.Controllers
+{
+    [Route("api/usuario")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private static List<Usuario> usuarios = new List<Usuario>();
+
+        [HttpGet]
+        [Route("buscarTodosUsuarios")]
+        public List<Usuario> buscarTodosUsuarios()
+        {
+            return usuarios;
+        }
+
+        [HttpPost]
+        [Route("adicionarNovoUsuario")]
+        public Usuario adicionarNovoUsuario([FromBody]Usuario usuario)
+        {
+            usuarios.Add(usuario);
+            return usuario;
+        }
+
+        [HttpDelete]
+        [Route("removerUsuario")]
+        public void removerUsuario([FromBody]Usuario usuario)
+        {
+            usuarios.RemoveAt(usuarios.IndexOf(usuarios.First(x => x.Equals(usuario))));
+        }
+    }
+}
---
+++ Api/ApiAmma/ApiAmma/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAmma.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string senha { get; set; }
+        public int codAvatar { get; set; }
+        public int codTema { get; set; }
+    }
+}
