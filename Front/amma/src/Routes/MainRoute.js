92dd6fd3ae40f0986f1c96cdbf4dcbfc18b15e8b
Keven <kevenbarauna@gmail.com>
2020-09-12 11:47:46 -0300

    WIP: Component Loading

--- Front/amma/src/Routes/MainRoute.js
+++ Front/amma/src/Routes/MainRoute.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from './../Pages/Home/Home';
 import Login from './../Pages/Login/Login';
+import Loading from './../Components/Carregando/Loading';
 
 const MainRoute = () => {
 
     return (
         < BrowserRouter >
+            <Loading />
             <Switch>
                 <Route path='/' component={Home} exact />
                 <Route path={'/home'} component={Home} />
                 <Route path={'/login'} component={Login} />
-                {/* <Route path={link.pageNotFound} component={Page404} /> */}
-                {/* <Redirect to={link.pageNotFound} /> */}
+                <Route path={'/404'} component={Loading} />
+                <Redirect to={'/404'} />
             </Switch>
         </BrowserRouter >
     );
