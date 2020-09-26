fa5b0d82e02799bc14ea30c2a91bbbaa883aab21
Keven <kevenbarauna@gmail.com>
2020-09-26 19:38:07 -0300

    add Const rotas

---
+++ Front/amma/src/Helpers/Const/Links.js
@@ -0,0 +1,35 @@
+export default class Link {
+
+    static get pageNotFound() {
+        return '/404';
+    }
+
+    static get home() {
+        return '/home';
+    }
+
+    static get login() {
+        return '/login';
+    }
+
+    static get dashboard() {
+        return '/dashboard';
+    }
+
+    static get Sugestões() {
+        return '/sugestoes';
+    }
+
+    static get Sobre() {
+        return '/sobre';
+    }
+
+    static get Avisos() {
+        return '/avisos';
+    }
+
+    static get Adm() {
+        return '/adm';
+    }
+
+}
