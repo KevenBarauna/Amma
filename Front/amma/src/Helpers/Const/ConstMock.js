ca72b8efa0d8751c3a017f62c9965e9afe3db12f
Keven <kevenbarauna@gmail.com>
2020-09-26 17:10:44 -0300

    Ajuste tipo ticket

--- Front/amma/src/Helpers/Const/ConstMock.js
+++ Front/amma/src/Helpers/Const/ConstMock.js
@@ -1,12 +1,12 @@
 export const TOP_TICKET_PERFIL = [
-    { tipoTicket: { id: '1', nome: 'Amar' }, titulo: 'Vaga interna', data: '13/09/2020' },
+    { tipoTicket: { id: '1', nome: 'Adicionar' }, titulo: 'Vaga interna', data: '13/09/2020' },
     { tipoTicket: { id: '2', nome: 'Manter' }, titulo: 'Home office', data: '07/08/2020' },
     { tipoTicket: { id: '3', nome: 'Melhorar' }, titulo: 'Armazenamento', data: '10/09/2020' },
     { tipoTicket: { id: '4', nome: 'Abandonar' }, titulo: 'Reunião 8h', data: '11/10/2020' },
 ];
 
 export const TiposTicketMock = [
-    { id: '1', nome: 'Amar' },
+    { id: '1', nome: 'Adicionar' },
     { id: '2', nome: 'Manter' },
     { id: '3', nome: 'Melhorar' },
     { id: '4', nome: 'Abandonar' }
