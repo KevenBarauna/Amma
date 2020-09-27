443cafd3faaf933ff137d5dea27f9dcdc8b22af7
Keven <kevenbarauna@gmail.com>
2020-09-26 22:33:04 -0300

    Perfil statico

--- Front/amma/src/Components/Perfil/Perfil.js
+++ Front/amma/src/Components/Perfil/Perfil.js
@@ -1,39 +1,31 @@
 import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import './Perfil.css';
 import imagemTemp from './../../assets/image/temp2.png';
-import imagemMenu from './../../assets/image/menu-icone.svg';
 import TopFivePerfil from './../TopFivePerfil/TopFivePerfil';
 
 
 const Perfil = () => {
 
     return (
-        <Row>
-            <Col sm={2} className='perfil-coluna-principal'>
-                <input type='checkbox' id='chec' className='perfilT-checkbox' />
-                <label htmlFor='chec' className='perfilT-label'>
-                    <img src={imagemMenu} alt='Perfil' style={{ height: '30px', cursor: 'pointer' }} />
-                </label>
-                <Col id='perfil-teste' className='perfil-coluna-sub-principal'>
-                    <div className='perfil-container'>
-                        <div className='perfil-imagem-jogador'>
-                            <img className='perfil-imagem' src={imagemTemp} alt='Menu' />
-                        </div>
-                        <div className='perfil-nome'>
-                            Keven Baraúna
-                        </div>
-                        <div className='perfil-cargo'>
-                            Coder II
-                        </div>
-                        <div>
-                            <TopFivePerfil />
-                        </div>
+        <Col sm={2} className='perfil-coluna-principal'>
+            <Col id='perfil-teste' className='perfil-coluna-sub-principal'>
+                <div className='perfil-container'>
+                    <div className='perfil-imagem-jogador'>
+                        <img className='perfil-imagem' src={imagemTemp} alt='Menu' />
                     </div>
-                </Col>
-
+                    <div className='perfil-nome'>
+                        Keven Baraúna
+                        </div>
+                    <div className='perfil-cargo'>
+                        Coder II
+                        </div>
+                    <div>
+                        <TopFivePerfil />
+                    </div>
+                </div>
             </Col>
-        </Row >
+        </Col>
     );
 }
 
