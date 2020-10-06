56993e7b394abca119608a83cc8e9cb8b09d298e
Keven Baraúna <kpbarauna>
2020-10-06 12:52:52 -0300

    Updates

---
+++ precache-manifest.f9571e95557c3784a14e35ee65888abe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83de1888211c5f316bc90c405bdabef2",
+    "url": "/Amma/index.html"
+  },
+  {
+    "revision": "3753a57e0cd8d3454ab4",
+    "url": "/Amma/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c3ae21eaa89d33fdfb2d",
+    "url": "/Amma/static/css/main.b30257bb.chunk.css"
+  },
+  {
+    "revision": "3753a57e0cd8d3454ab4",
+    "url": "/Amma/static/js/2.92bea6f1.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/Amma/static/js/2.92bea6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ae21eaa89d33fdfb2d",
+    "url": "/Amma/static/js/main.9a4d58d7.chunk.js"
+  },
+  {
+    "revision": "bb655d2ee3fb022e6567",
+    "url": "/Amma/static/js/runtime-main.2c491f3c.js"
+  },
+  {
+    "revision": "35dc4816d23601e5b75a44da15e3d18c",
+    "url": "/Amma/static/media/background-adicionar.35dc4816.jpg"
+  },
+  {
+    "revision": "5336c4fcc10c8c587f1d868ef1202adb",
+    "url": "/Amma/static/media/background-login.5336c4fc.jpg"
+  },
+  {
+    "revision": "74929503d0dbab88aac5e3fe3f883b1f",
+    "url": "/Amma/static/media/logo.74929503.png"
+  },
+  {
+    "revision": "d17d014a6b32c45a87a9cc5312fecdfd",
+    "url": "/Amma/static/media/sino.d17d014a.png"
+  },
+  {
+    "revision": "514825b8f182f839f5d669dac8cd4fd1",
+    "url": "/Amma/static/media/temp2.514825b8.png"
+  }
+]);
